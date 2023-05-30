94a901eecc2835cf578db1bd513f668b89b76a99
Karpenko21 <irinakrivosheeva55@gmail.com>
2023-05-30 09:33:09 +0300

    Counter after exam

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {Counter} from "./component/Counter";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [value, setValue] = useState(0)
+
+    return (
+        <div className="App">
+            <Counter value={value} onClick={setValue}/>
+        </div>
+    );
 }
 
 export default App;
